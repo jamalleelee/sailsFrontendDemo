50374ea04d36a6cabeb82b6e2f4ecd8f1162d2d4
Jamal <jamal.l.mcdowell@gmail.com>
2016-09-09 16:55:17 -0400

    changed wedsite format, completed CRUD

--- api/controllers/ViddyGameController.js
+++ api/controllers/ViddyGameController.js
@@ -11,64 +11,122 @@ var endpoint = "http://localhost:1337/viddygames"
 
 module.exports = {
 
-  /**
-   * `ViddyGameController.create()`
-   */
-  create: function (req, res) {
+    /**
+     * `ViddyGameController.create()`
+     */
+    create: function(req, res) {
 
-        if(req.method != "POST"){
-          return res.view('create');
+      if (req.method != "POST") {
+        return res.view('create');
+      }
+
+      var args = {
+        data: req.body,
+        headers: {
+          "Content-Type": "application/json"
+        }
+      };
+
+      client.post(endpoint, args, function(data, response) {
+        // return res.view('create', {success: { message: "Record added successfully"}});
+        if (response.statusCode != "201") {
+          return res.view('create', {
+            error: {
+              message: response.statusMessage + ": " + data.reason
+            }
+          });
         }
 
-        var args = {
-            data: req.body,
-            headers: { "Content-Type": "application/json" }
-        };
+        return res.redirect('/view');
 
-        client.post(endpoint, args, function (data, response) {
+
+      })
+
+    },
+
+
+    /**
+     * `ViddyGameController.read()`
+     */
+    read: function(req, res) {
+
+      client.get(endpoint, function(data, response) {
+        return res.view('read', {
+          viddygames: data
+        });
+      }).on('error', function(err) {
+        return res.view('read', {
+          error: {
+            message: "There was an error getting the records"
+          }
+        });
+      });
+
+    },
+
+
+    /**
+     * `ViddyGameController.update()`
+     */
+    update: function(req, res) {
+      client.get(endpoint, function(data, response) if (req.method != "PUT") {
+            return res.view('update');
+          }
+
+          var args = {
+            data: req.body,
+            headers: {
+              "Content-Type": "application/json"
+            }
+          };
+
+          client.PUT(endpoint, args, function(data, response) {
             // return res.view('create', {success: { message: "Record added successfully"}});
-            if(response.statusCode != "201"){
-                return res.view('create', {error:{message: response.statusMessage + ": " + data.reason}});
+            if (response.statusCode != "200") {
+              return res.view('update', {
+                error: {
+                  message: response.statusMessage + ": " + data.reason
+                }
+              });
             }
 
-            return res.view('read', {success:{message: "Record created successfully"}});
+            return res.redirect('/view');
 
 
-        })
+          })
 
-  },
+        },
+        /**
+         * `ViddyGameController.delete()`
+         */
+        delete: function(req, res) {
+
+          if (req.method != "POST") {
+            return res.view('delete');
+          }
+
+          var args = {
+            data: req.body,
+            headers: {
+              "Content-Type": "application/json"
+            }
+          };
+
+          client.delete(endpoint, args, function(data, response) {
+            // return res.view('create', {success: { message: "Record added successfully"}});
+            if (response.statusCode != "200") {
+              return res.view('delete', {
+                error: {
+                  message: response.statusMessage + ": " + data.reason
+                }
+              });
+            }
+
+            return res.redirect('/view');
+          })
 
 
-  /**
-   * `ViddyGameController.read()`
-   */
-  read: function (req, res) {
-
-    client.get(endpoint, function (data, response) {
-        return res.view('read', {viddygames: data});
-    }).on('error', function (err) {
-        return res.view('read', {error: { message: "There was an error getting the records"}});
-    });
-
-  },
+        }
 
 
-  /**
-   * `ViddyGameController.update()`
-   */
-  update: function (req, res) {
-    return res.json({
-      todo: 'update() is not implemented yet!'
-    });
-  },
-
-
-  /**
-   * `ViddyGameController.delete()`
-   */
-  delete: function (req, res) {
-    return res.json({
-      todo: 'delete() is not implemented yet!'
-    });
-  }
-};
+    };
